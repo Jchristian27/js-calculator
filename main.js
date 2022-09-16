38b7e8612161e23c2c14c39560036524c47276c2
Jonathan Christian <christianj707@gmail.com>
2022-09-16 16:19:02 -0700

    Remove #s from id selectors

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const display = document.querySelector('.display');
-const one = document.getElementById('#one');
-const two = document.getElementById('#two');
-const three = document.getElementById('#three');
-const four = document.getElementById('#four');
-const five = document.getElementById('#five');
-const six = document.getElementById('#six');
-const seven = document.getElementById('#seven');
-const eight = document.getElementById('#eight');
-const nine = document.getElementById('#nine');
-const zero = document.getElementById('#zero');
-const point = document.getElementById('#point');
-const plus = document.getElementById('#plus');
-const minus = document.getElementById('#subtract');
-const times = document.getElementById('#multiply');
-const divides = document.getElementById('#divide');
-const equals = document.getElementById('#equals');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const zero = document.getElementById('zero');
+const point = document.getElementById('point');
+const plus = document.getElementById('plus');
+const minus = document.getElementById('subtract');
+const times = document.getElementById('multiply');
+const divides = document.getElementById('divide');
+const equals = document.getElementById('equals');
 const clear = document.querySelector('.clear');
 const deletes = document.querySelector('.delete');
 
@@ -46,6 +46,6 @@ clear.addEventListener('click', function() {
   display.innerHTML = "<h2>0</h2>";
 });
 
-two.addEventListener("click", function() {
+two.addEventListener('click', function() {
   display.innerHTML = "<h2>1</h2>";
 })
